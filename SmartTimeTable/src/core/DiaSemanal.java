1a991b16e848cdd2242b507082e2d4431031cfd8
André Alves <andr.alves@ua.pt>
2020-09-20 18:54:47 +0100

    Added Saturday in TimeTable

--- SmartTimeTable/src/core/DiaSemanal.java
+++ SmartTimeTable/src/core/DiaSemanal.java
@@ -6,5 +6,8 @@ import java.io.Serializable;
  * An enumerator with the days of the week.
  * @author Diogo Regateiro
  *
+ * @author andralves
+ * @version 4.6
+ *
  */
-public enum DiaSemanal implements Serializable { Monday, Tuesday, Wednesday, Thursday, Friday }
+public enum DiaSemanal implements Serializable { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday }
